61597abc0a874c55bef1fc81eba3d3bfabad53d1
Kts <k.tsimbarenko@starladder.tv>
2019-02-20 18:48:57 +0200

    Test3

---
+++ Test3.py
@@ -0,0 +1,9 @@
+# a = input("Введите строку ")
+# print(a[2], a[-2], a[0:5], a[:-2], a[0::2], a[1::2], a[::-1], a[-1::-2], a[-2:0:-1], len(a), sep='\n')
+# a = input("Введите строку ")
+# if len(a) % 2 == 0:
+#     b = a[:int(len(a)/2)]
+#     print(a[int(len(a)/2):]+b)
+# else:
+#     b = a[:int(len(a)/2+1)]
+#     print(a[int(len(a)/2+1):]+b)

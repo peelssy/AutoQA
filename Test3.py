c5ef346e6d995f8e0f9f1274034314addc00027f
Kts <k.tsimbarenko@starladder.tv>
2019-02-20 19:25:08 +0200

    Changes Test3

--- Test3.py
+++ Test3.py
@@ -7,3 +7,23 @@
 # else:
 #     b = a[:int(len(a)/2+1)]
 #     print(a[int(len(a)/2+1):]+b)
+# x = 0
+# while x <= 10:
+#     print(x, end=' ')
+#     x += 1
+# x = 20
+# while x >= 1:
+#     print(x, end=' ')
+#     x -= 1
+
+# x = int(input())
+# i = 0
+# while x % 2 == 0:
+#     x = x / 2
+#     i += 1
+# print(i)
+
+l = [1, 2, 3, 4, 5, 6, 7, 8]
+for a in l:
+    l.remove(a)
+    print(l)
